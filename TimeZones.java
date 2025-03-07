7e52a91700989817705b5e1e3011f6fda84ba603
Akki <chauhanakshit61@gmail.com>
2025-03-07 21:41:46 +0530

    [Akshit] ADD: Getting time from different time zones

---
+++ TimeZones.java
@@ -0,0 +1,22 @@
+import java.time.ZonedDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public class TimeZones {
+    public static void main(String[] args) {
+        // Define a formatter for displaying time
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm a, dd MMM yyyy");
+
+        // Get current time in GMT (Greenwich Mean Time)
+        ZonedDateTime gmtTime = ZonedDateTime.now(ZoneId.of("GMT"));
+        System.out.println("Current Time in GMT: " + gmtTime.format(formatter));
+
+        // Get current time in IST (Indian Standard Time)
+        ZonedDateTime istTime = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
+        System.out.println("Current Time in IST: " + istTime.format(formatter));
+
+        // Get current time in PST (Pacific Standard Time)
+        ZonedDateTime pstTime = ZonedDateTime.now(ZoneId.of("America/Los_Angeles"));
+        System.out.println("Current Time in PST: " + pstTime.format(formatter));
+    }
+}
