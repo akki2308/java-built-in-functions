455f6817c095f3425abac7567070cefbecd106d9
Akki <chauhanakshit61@gmail.com>
2025-03-07 21:35:37 +0530

    [Akshit] ADD: Comparing two dates

---
+++ DateComparison.java
@@ -0,0 +1,27 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class DateComparison {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking the first date input from the user
+        System.out.print("Enter the first date (YYYY-MM-DD): ");
+        LocalDate date1 = LocalDate.parse(sc.next());
+
+        // Taking the second date input from the user
+        System.out.print("Enter the second date (YYYY-MM-DD): ");
+        LocalDate date2 = LocalDate.parse(sc.next());
+
+        // Comparing the dates
+        if (date1.isBefore(date2)) {
+            System.out.println("The first date is before the second date.");
+        } else if (date1.isAfter(date2)) {
+            System.out.println("The first date is after the second date.");
+        } else {
+            System.out.println("Both dates are the same.");
+        }
+
+        sc.close();
+    }
+}
